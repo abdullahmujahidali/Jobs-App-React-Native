5455642e0657dd93466031990fb099844984814c
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-03-19 09:25:04 +0500

    added changes

--- app/_layout.js
+++ app/_layout.js
@@ -1,28 +1,30 @@
-import {Stack} from 'expo-router';
-import { useCallback } from 'react';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
 
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "home",
+};
 
-const Layout = () =>{
-    const [loaded] = useFonts({
-        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
-    });
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
 
-    const onLayoutRootView = useCallback(async () => {
-        if(loaded){
-            await SplashScreen.hideAsync();
-        }
-    },[loaded]);
+  if (!fontsLoaded) {
+    return null;
+  }
 
-    if(!loaded){
-        return null;
-    }
-    return <Stack onLayout={onLayoutRootView}/>;
-}
+  return (
+    <Stack initialRouteName="home">
+      <Stack.Screen name="home" />
+    </Stack>
+  )
+};
 
 export default Layout;
